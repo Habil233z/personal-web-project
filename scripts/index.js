4ad28f8a951f8c0cf0ea91f32c3aa31dcf081c66
habil <habilherdianto233z@gmail.com>
2026-04-01 17:19:07 +0700

    with dummydata not working properly

--- scripts/index.js
+++ scripts/index.js
@@ -17,50 +17,70 @@ function back() {
 //     },
 // ];
 let projectsList = []
+
+const dummyData = [
+        {
+         id:2,
+         name: "placeholder 1",
+         description: "wadhawjwjadhjkawhjdhkawjhdkjhwashduiwhaduaw",
+        img: "../src/Img_1.jpg"
+        },
+         {
+        id:3,
+        name: "placeholder 2",
+        description: "awdawudhauiwhduiawhuidhuawduiawiudhjiawjdioawiodjhioawhdioawh",
+        img: "../src/Img_2.jpg"
+  }
+]
+
+  console.log(projectsList.length)
 const projects = localStorage.getItem('projects');
     if (projects){
         projectsList = JSON.parse(projects);
+        projectsList.push(...dummyData)
     }
 
 
 console.log(projectsList)
 
-function renderProjects() {
-    const projectsContainer = document.getElementById("containerArea")
 
-    let projectsHTML = "";
-    
-    for (let i=0; i < projectsList.length; i++) {
-        const project = projectsList[i];
+    function renderProjects() {
+        if (projectsList.length !== 0) {
+        const projectsContainer = document.getElementById("containerArea")
+
+        let projectsHTML = "";
         
-        let projectOverview = project.description
-        let intro = projectOverview.slice(0, 100)
-        console.log(intro)
+        for (let i=0; i < projectsList.length; i++) {
+            const project = projectsList[i];
+            
+            let projectOverview = project.description
+            let intro = projectOverview.slice(0, 100)
+            console.log(intro)
 
-        projectsHTML += `
-        <div class="card" id="card" style="width: 18rem;">
-              <img class="card-img-top" src="${project.img}" alt="${project.img}">
-              <div class="card-body">
-                  <h5 class="card-title">${project.name}</h5>
-                  <p class="card-text">${intro}</p>
-                  <button id="deleteButton" >Delete</button>
-                  <button id="detailButton">Detail</button>
-                    <div class="detail" id="detail">
-                        <div class="detailContainer">
-                        <img class="imgDetail" src="${project.img}" alt="${project.img}">
-                        <p>${project.description}</p>
-                        <button id="closeDetail">Close</button>
+            projectsHTML += `
+            <div class="card" id="card" style="width: 18rem;">
+                <img class="card-img-top" src="${project.img}" alt="${project.img}">
+                <div class="card-body">
+                    <h5 class="card-title">${project.name}</h5>
+                    <p class="card-text">${intro}</p>
+                    <button id="deleteButton" >Delete</button>
+                    <button id="detailButton">Detail</button>
+                        <div class="detail" id="detail">
+                            <div class="detailContainer">
+                            <img class="imgDetail" src="${project.img}" alt="${project.img}">
+                            <p>${project.description}</p>
+                            <button id="closeDetail">Close</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    
+                </div>`
                 
-            </div>`
-            
-    }
-    projectsContainer.innerHTML = projectsHTML
-}
-
-renderProjects();
+        }projectsContainer.innerHTML = projectsHTML
+    }else {
+        console.log("No Data")
+    }}
+    renderProjects()
 
 let base64String = "";
 
@@ -101,23 +121,23 @@ form.addEventListener("submit", function(event){
     renderProjects();
 })
 
-let i = 0
+    let i = 0
 
-document.getElementById("deleteButton").addEventListener("click", function() {
-projectsList.splice(i, 2);
-localStorage.setItem('projects', JSON.stringify(projectsList));
-renderProjects(); 
-});
+    document.getElementById("deleteButton").addEventListener("click", function() {
+    projectsList.splice(i, 2);
+    localStorage.setItem('projects', JSON.stringify(projectsList));
+    renderProjects(); 
+    });
 
 
-const detaiButton = document.getElementById("detailButton")
-const closeDetail = document.getElementById("closeDetail")
-const detail = document.getElementById("detail")
+    const detaiButton = document.getElementById("detailButton")
+    const closeDetail = document.getElementById("closeDetail")
+    const detail = document.getElementById("detail")
 
-detaiButton.addEventListener("click", () => {
-detail.classList.add("open");
-});
+    detaiButton.addEventListener("click", () => {
+    detail.classList.add("open");
+    });
 
-closeDetail.addEventListener("click", () => {
-detail.classList.remove("open")
-})
+    closeDetail.addEventListener("click", () => {
+    detail.classList.remove("open")
+    })
